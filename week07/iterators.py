5ead4fe37eb5b7d19bc4ea1a87b99f28bd01554b
alexandriacripps <ali12182004@gmail.com>
2022-10-28 10:14:23 -0400

    Week 7

---
+++ week07/iterators.py
@@ -0,0 +1,8 @@
+for spaces in range(0,10):
+    for col in range(0,spaces):
+        print(" ", end=" ")
+    print("Z")
+for spaces in range(0,10):
+    for col in range(0,spaces):
+        print(" ", end=" ")
+    print("+")
